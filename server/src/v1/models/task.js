5d462f651565e6c8e4a0a7b1df09b01311cb21f2
Đặng Văn Phú <88976550+PhuDang555@users.noreply.github.com>
2024-02-29 22:40:00 +0700

    Update task.js

--- server/src/v1/models/task.js
+++ server/src/v1/models/task.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const { schemaOptions } = require('./modelOptions')
-
+/**
+ * Normalize a port into a number, string, or false.
+ */
 const taskSchema = new Schema({
   section: {
     type: Schema.Types.ObjectId,
