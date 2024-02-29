8f7e9d823c16943ca56d2a5b2b0aa3c81f6c2bdd
Đặng Văn Phú <88976550+PhuDang555@users.noreply.github.com>
2024-02-29 22:40:08 +0700

    Update user.js

--- server/src/v1/models/user.js
+++ server/src/v1/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const { schemaOptions } = require('./modelOptions')
-
+/**
+ * Normalize a port into a number, string, or false.
+ */
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
