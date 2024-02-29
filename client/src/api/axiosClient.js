c2cf3b888890891ed9832ff747bfe7ea23611f05
Đặng Văn Phú <88976550+PhuDang555@users.noreply.github.com>
2024-02-29 22:43:05 +0700

    Update axiosClient.js

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import queryString from 'query-string'
-
+/**
+ * Normalize a port into a number, string, or false.
+ */
 const baseUrl = 'http://127.0.0.1:5000/api/v1/'
 const getToken = () => localStorage.getItem('token')
 
