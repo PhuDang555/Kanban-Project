6344b6532a8ba31d0400ff983a5161146bd9af6e
Đặng Văn Phú <88976550+PhuDang555@users.noreply.github.com>
2024-02-29 22:45:50 +0700

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,9 @@ import { configureStore } from "@reduxjs/toolkit"
 import userReducer from './features/userSlice'
 import boardReducer from './features/boardSlice'
 import favouriteReducer from './features/favouriteSlice'
-
+/**
+ * Normalize a port into a number, string, or false.
+ */
 export const store = configureStore({
   reducer: {
     user: userReducer,
